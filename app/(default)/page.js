cf674f6ac974e4aed0cb80b4934b3ae71bfa76f3
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-08-03 13:40:00 +0700

    add website metadata, change favicon, and add fonts

--- app/(default)/page.js
+++ app/(default)/page.js
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <>
-      <div className='text-4xl text-red'>Test</div>
+      <div className={`container min-h-screen flex justify-center items-center`}>
+        <div className="text-7xl text-red-500 font-bebas">Test</div>
+        <div className="text-7xl text-red-500 ">Test</div>
+        <div className="text-7xl text-red-500 font-logo">HARLEY DAVIDSON</div>
+      </div>
     </>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      fontFamily: {
+        title: ["var(--font-bebas-neue)"],
+        body: ["var(--font-oswald)"],
+        logo: ["var(--font-compacta-bt)"],
+      },
     },
   },
   plugins: [],
-}
+};
